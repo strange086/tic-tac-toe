42cdefc1983a63ba5b1969b399a4d62cc17cec7f
strange086 <akashachu0000@gmail.com>
2025-04-20 12:15:02 +0530

    Updates

---
+++ precache-manifest.1ecfde03cec5afcb242dbd2af942c1e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9fc9977f86a9a7b95cd150c303d0b4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7afb420d32e4ca0bcfb0",
+    "url": "/tic-tac-toe/static/css/main.be4bd08c.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/tic-tac-toe/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "7afb420d32e4ca0bcfb0",
+    "url": "/tic-tac-toe/static/js/main.fe8e0645.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
